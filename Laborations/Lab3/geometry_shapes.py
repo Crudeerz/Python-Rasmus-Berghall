afdd776a9fe54334571f8da973bd161faf9693f0
Crudeerz <rassefhc@yahoo.se>
2023-10-13 17:37:32 +0200

    Added Sphere class with functionality

--- Laborations/Lab3/geometry_shapes.py
+++ Laborations/Lab3/geometry_shapes.py
@@ -9,7 +9,7 @@ class Circle:
     When initializing a Circle you should enter the circles middlepoint in a 2d coordinate system
     and the length of the sides.
 
-    When comparing the Circle we are comparing area and ignoring the middlepoint (x,y)
+    When using any comparing operator for the Circle we are comparing area and ignoring the middlepoint (x,y)
 
                 Parameters
                             x:       (int/float)
@@ -108,7 +108,7 @@ class Rectangle:
     When initializing a Rectangle you should enter the rectangles middlepoint in a 2d coordinate system
     and the length of the sides.
 
-    When comparing the Rectangle we are comparing area and ignoring the middlepoint (x,y)
+    When using any comparing operator for the Rectangle we are comparing area and ignoring the middlepoint (x,y)
 
                 Parameters
                             x:       (int/float)
@@ -200,7 +200,7 @@ class Cube:
     When initializing a Cube you should enter the cubes middlepoint in a 3d coordinate system
     and the length of the sides.
 
-    When comparing the Cube we are comparing volume and ignoring the middlepoint (x,y,z)
+    When using any comparing operator for the Cube we are comparing volume and ignoring the middlepoint (x,y,z)
 
                 Parameters
                             x:       (int/float)
@@ -271,10 +271,97 @@ class Cube:
     
     def is_inside(self, x, y, z):
         check_int_float(x, y, z)
-        
+
         # Kolla om punkten ligger innanför instansen för objektet
         if  (((self._x - self._side/2) < x < (self._x + self._side/2)) and 
              ((self._y - self._side/2) < y < (self._y + self._side/2)) and
              ((self._z - self._side/2) < z < (self._z + self._side/2))): 
             return True  
         else: return False
+
+
+class Sphere:
+    '''
+    Class for Sphere-object
+
+    When initializing a Sphere you should enter the spheres middlepoint in a 3d coordinate system
+    and the radius af the spere.
+
+    When using any comparing operator for the Sphere we are comparing volume and ignoring the middlepoint (x,y,z)
+
+                Parameters
+                            x:       (int/float)
+                            y:       (int/float)
+                            z:       (int/float)
+                            radius:    (int/float)
+    
+    '''
+    def __init__(self, x, y, z, radius):
+        check_int_float(x, y, z, radius)
+        self._x = x
+        self._y = y
+        self._z = z
+        self._radius = radius
+
+    def __repr__(self):
+        return f"x = {self._x}, y = {self._y}, z = {self._z}, radius = {self._radius}"
+    
+    def __str__(self):
+        return f"A sphere with the middlepoint: {self._x}, {self._y}, {self._z} and the radius: {self._radius}"
+
+    def __eq__(self, other):
+       if check_type_comparison(other, Sphere) is False: return False
+
+       if self.get_volume == (other if type(other) in [int, float] else other.get_volume): return True
+       else: return False
+
+       
+    def __le__(self, other):
+       if check_type_comparison(other, Sphere) is False: return False
+       
+       if self.get_volume <= (other if type(other) in [int, float] else other.get_volume): return True
+       else: return False 
+
+
+    def __ge__(self, other):
+        if check_type_comparison(other, Sphere) is False: return False
+       
+        if self.get_volume >= (other if type(other) in [int, float] else other.get_volume): return True
+        else: return False
+
+
+    def __lt__(self, other):
+       if check_type_comparison(other, Sphere) is False: return False
+       
+       if self.get_volume < (other if type(other) in [int, float] else other.get_volume): return True
+       else: return False 
+
+
+    def __gt__(self, other):
+       if check_type_comparison(other, Sphere) is False: return False
+       
+       if self.get_volume > (other if type(other) in [int, float] else other.get_volume): return True
+       else: return False
+
+
+    @property
+    def get_volume(self):
+        return (4 * pi * self._radius**3) / 3
+ 
+
+    def translate(self, x, y, z):
+        check_int_float(x, y, z)
+        
+        self._x = x
+        self._y = y
+        self._z = z
+    
+    def is_inside(self, x, y, z):
+        check_int_float(x, y, z)
+
+        # Beräkna distancen mellan punkterna och jämför med radien
+        # för att se om punkten är i cirkeln
+        dist = sqrt((self._x - x)**2 + (self._y - y)**2 + (self._z -z)**2)
+        return True if dist < self._radius else False
+        
+        
--- Laborations/Lab3/test_shapes.py
+++ Laborations/Lab3/test_shapes.py
@@ -1,4 +1,4 @@
-from geometry_shapes import Circle, Rectangle, Cube
+from geometry_shapes import Circle, Rectangle, Cube, Sphere
 
 cirkel1 = Circle(1,3,3)
 
@@ -13,7 +13,12 @@ rektangel1 = Rectangle(3, 4, 4, 5)
 rektangel2 = Rectangle(2, 6, 5, 5)
 
 kub1 = Cube(3, 4, 2, 4)
+
 kub2 = Cube(4, 2, 1, 4)
+
+sfär1 = Sphere(3,4,6,3)
+
+sfär2 = Sphere(3,4,6,4)
 
 # print(cirkel1.is_unitcircle)
 # print(cirkel4.is_unitcircle)
@@ -21,6 +26,9 @@ kub2 = Cube(4, 2, 1, 4)
 # print(rektangel2.is_square)
 
 # print(kub1.is_inside(4,5,3))
+
+print(sfär1.is_inside(2, 3, 3))
+
 
 
 
